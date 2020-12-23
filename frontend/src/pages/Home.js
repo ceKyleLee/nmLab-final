761de338d17fb4f4b29886665983d8f3529bf96b
MidLaneBro <chung.a40709@gmail.com>
2020-12-23 16:03:23 +0800

    adjust home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react';
 
 function Home (props){
     let accounts = props.accounts;
+    const [name,setname] = useState("")
     const [text,settext] = useState("")
+    const [type,settype] = useState("")
 
     const submit = async () => {
         let yes = window.confirm('Are you sure to submit?');
@@ -22,7 +24,9 @@ function Home (props){
     useEffect(()=>{
         async function fetchData(){
             let result = await props.contract.methods.getAccount(props.accounts[0]).call();
+            setname(result._name);
             settext(result._content);
+            settype(result._acc_type);
         }
         fetchData();
     },[]);
@@ -31,10 +35,11 @@ function Home (props){
         <div className="App">
             <div className="header">
                 <h1>去中心化人力銀行</h1>
+                <h3>Username:{name} Account Type:{type==="1"? "Personal":"Company"}</h3>
             </div>
             <div className="body">
                 <div className="left-box">
-                    <textarea className="context-input" maxLength="200" placeholder="type here" required></textarea>
+                    <textarea className="context-input" maxLength="200" placeholder={type==="1"? "type your resume...":"type company introduction..."} required></textarea>
                     <button className="context-submit" onClick={submit}>submit</button>
                 </div>
                 <div className="right-box">
